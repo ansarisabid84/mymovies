5932953c9ed8f7322423a285150ba37be2622f6c
Sabid Ansari <ansarisabid84@gmail.com>
2024-01-24 20:50:10 +0530

    Build a simple yet impactful movie app that  ill feature a list of trending movies and offer the capability to explore detailed information about each movie.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Building Modern Angular Apps with Ionic and Angular v17
+
+In Angular version 17, the Angular team has introduced groundbreaking changes that have also been seamlessly incorporated into Ionic. This tutorial will guide you through harnessing these new features to construct powerful and performant Angular apps with Ionic.
+
+## What You Will Learn
+
+By the end of this tutorial, you will be adept at:
+
+- Using the new Angular Control Flow
+- Working with Signals for efficient communication
+- Implementing Deferred Blocks for managing asynchronous operations
+- Importing Ionic Standalone Components
+
+## Project Overview
+
+We will embark on building a simple yet impactful movie app. This application will feature a list of trending movies and offer the capability to explore detailed information about each movie.
+
+Let's dive into the tutorial to discover how these enhancements in Angular v17 can elevate your Angular app development experience when coupled with the versatility of Ionic.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,90 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, InfiniteScrollCustomEvent, IonAlert, IonAvatar, IonBadge, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonList, IonLoading, IonSkeletonText } from '@ionic/angular/standalone';
+import { finalize, catchError } from 'rxjs';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonLabel,
+    IonBadge,
+    IonAvatar,
+    IonItem,
+    IonList,
+    IonLoading,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+    IonSkeletonText,
+    IonAlert,
+    DatePipe,
+    RouterModule,
+  ],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private movieService = inject(MovieService);
+
+  private currentPage = 1;
+  public movies: any[] = [];
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  public isLoading = true;
+  public error = null;
+  public dummyArray = new Array(5);
+
   constructor() {}
+
+  ngOnInit(): void {
+    this.loadMovies();
+      
+  }
+
+  async loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+
+    // Only show loading indicator on initial load
+    if (!event) {
+      this.isLoading = true;
+    }
+
+    // Get the next page of movies from the MovieService
+    this.movieService
+      .getTopRatedMovies(this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+        }),
+        catchError((err: any) => {
+          this.error = err.error.status_message;
+          return [];
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          // Append the results to our movies array
+          this.movies.push(...res.results);
+
+          // Resolve the infinite scroll promise to tell Ionic that we are done
+          event?.target.complete();
+
+          // Disable the infinite scroll when we reach the end of the list
+          if (event) {
+            event.target.disabled = res.total_pages === this.currentPage;
+          }
+        },
+      });
+  }
+
+  // This method is called by the infinite scroll event handler
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.currentPage++;
+    this.loadMovies(event);
+  }
 }
